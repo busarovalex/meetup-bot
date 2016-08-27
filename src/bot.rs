1217aecbf54c5815d3e106a384a73c3c06e5138a
Alex <busarovalex@yandex.ru>
2016-08-27 16:48:06 +0300

    Удалены комментарии

--- src/bot.rs
+++ src/bot.rs
@@ -41,37 +41,4 @@ impl<'r> Bot<'r> {
             self.api.send(error);
         }
     }
-    
-//    fn process_command(&mut self, command: Command, user: User, chat: Chat) {
-//        match command {
-//            Command::JoinMeetup(secret_phrase) => self.join_meetup(secret_phrase, user, chat),
-//            Command::LeaveMeetup(Some(secret_phrase)) => self.leave_meetup(secret_phrase, user, chat),
-//            _ => {}
-//        }
-//    }
-    
-//    fn join_meetup(&mut self, secret_phrase: String, user: User, chat: Chat) {
-//        let meetup = self.manager.create_or_find(&secret_phrase[..]);
-//        meetup.add_user(user);
-//        let response = format!("You joined {} meetup!", &secret_phrase);
-//        self.api.send_message(chat.id(), response, None, None, None, None).unwrap();
-//    }
-//    
-//    fn leave_meetup(&mut self, secret_phrase: String, user: User, chat: Chat) {
-//        if let Some(ref mut meetup) = self.manager.find(&secret_phrase[..]) {
-//            meetup.remove_user(&user);
-//            let response = format!("You left {} meetup!", &secret_phrase);
-//            self.api.send_message(chat.id(), response, None, None, None, None).unwrap();
-//        } else {
-//            let response = format!("You do not belong to {} meetup!", &secret_phrase);
-//            self.api.send_message(chat.id(), response, None, None, None, None).unwrap();
-//        }
-//    }
-    
-//    fn extract_command(msg: &TelegramMessage) -> Option<Command> {
-//        if let MessageType::Text(ref payload) = msg.msg {
-//            return Some(Command::from(&payload[..]));
-//        }
-//        None
-//    }
 }
